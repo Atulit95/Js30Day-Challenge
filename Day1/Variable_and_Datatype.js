9cda3294942b050e641b69503d6f809ce613066d
Atulit95 <atulitgupta436@gmail.com>
2024-07-13 22:09:48 +0530

    day1

---
+++ Day1/Variable_and_Datatype.js
@@ -0,0 +1,43 @@
+// ====================>Activitiy 1: Variable Declaration
+// Task 1:
+var num1 = 2
+console.log(num1);
+
+// Task2:
+let str1 = "Task2"
+console.log(str1);
+
+// ====================>Activitiy 2: Constant Declaration
+// Task3:
+const IsloggedIn = true
+console.log(IsloggedIn);
+
+// ====================>Activitiy 3: Data Types
+// Task4:
+
+let num2 = 3
+console.log(typeof num2);
+let str2 = "Task4"
+console.log(typeof str2);
+let taskCompleted = true
+console.log(typeof taskCompleted);
+let user = {
+    name: "Indiana Jones",
+    age: 12,
+    gender: "male"
+}
+console.log(typeof user);
+let myArr = [0,1,2,"3",true]
+console.log(typeof myArr);
+
+// ====================>Activitiy 4: Reassigning Varaibles
+// Task5:
+let new_variable = 53
+new_variable = 760
+console.log(new_variable);
+
+// ====================>Activitiy 5: Reassigning Varaibles
+// Task5:
+const new_variable2 = "Constant Variable"
+// new_variable2 = "Changed variable"
+console.log(new_variable2);// gives error for changing 'const' type variable
