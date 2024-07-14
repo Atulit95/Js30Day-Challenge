3bdd99cb23e6b6052b5f56e5ba03da07ef7cced8
Atulit95 <atulitgupta436@gmail.com>
2024-07-14 21:40:40 +0530

    day2

---
+++ Day2/operators.js
@@ -0,0 +1,51 @@
+// ====================>Activitiy 1: Arithmetic Operations
+// Task 1-5:
+let num1 = 570
+let num2 = 456
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+// ====================>Activitiy 2: Assignment Operator
+// Task 6-7:
+num1 += 70
+console.log(num1);
+num2 -= 56
+console.log(num2);
+
+// ====================>Activitiy 3: Comparison Operator
+// Task 8:
+console.log(num1 > num2);
+console.log(num1 < num2);
+
+// Task 8:
+console.log(num1 >+ num2);
+console.log(num1 <= num2);
+
+// Task 8:
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+let num3 = '640'
+console.log(num1 === num3);
+
+// ====================>Activitiy 4: Logical Operator
+// Task 11:
+console.log("x");
+console.log(num1 == 640 && num2 == 400);
+console.log(num1 == 640 && num2 == 470);
+
+// Task 12:
+console.log(num1 == 640 || num2 == 400);
+console.log(num1 == 640 || num2 == 470);
+
+// Task 13:
+console.log(num1 != 640);
+
+// ====================>Activitiy 5: Logical Operator
+// Task 14:
+let num4 = -45
+num4 >= 0 ? console.log("The number is positive number") : console.log("The number is negative number");
