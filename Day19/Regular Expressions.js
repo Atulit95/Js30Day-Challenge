0063ea6bc50a27f6fd4187130ec44f46fe43c358
Atulit95 <atulitgupta436@gmail.com>
2024-08-02 14:27:36 +0530

    day19

---
+++ Day19/Regular Expressions.js
@@ -0,0 +1,24 @@
+// ====================>Activity 1: Basic Regular Expression
+// Task 1:
+
+// Task 2:
+
+// ====================>Activity 2: Character Classes and Quantifiers
+// Task 3:
+
+// Task 4:
+
+// ====================>Activity 3: Grouping and Capturing
+// Task 5:
+
+// Task 6:
+
+// ====================>Activity 4: Assertions and Boundaries
+// Task 7:
+
+// Task 8:
+
+// ====================>Activity 5: Practical Applications
+// Task 9:
+
+// Task 10:
